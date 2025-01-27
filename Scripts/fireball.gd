2b2da8673b2049e808648b4007138d3049bf9001
minty <eithanmathewmalonzo@gmail.com>
2025-01-27 16:02:29 +0800

    Added projectile shooting function

---
+++ Scripts/fireball.gd
@@ -0,0 +1,19 @@
+extends Area3D
+
+@export var speed = 10
+@export var damage = 200
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	add_to_group("player")
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var dir = get_tree().get_first_node_in_group("player").global_transform.basis.z.normalized()
+	global_position += dir * speed * delta
+
+
+func _on_body_entered(body: Node3D) -> void:
+	queue_free()
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -3,16 +3,26 @@ extends CharacterBody3D
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
+#Fireball Instance
+const FIREBALL = preload("res://Scenes/fireball.tscn")
 
+#Fireball marker
+@onready var marker_3d = $Marker3D
+
+@onready var player = $"."
 
 func _physics_process(delta: float) -> void:
+
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
+		
+	if Input.is_action_just_pressed("ui_accept"):
+		_shoot_fireball()
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		#velocity.y = JUMP_VELOCITY
+		
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -26,3 +36,8 @@ func _physics_process(delta: float) -> void:
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
+
+func _shoot_fireball():
+	var fireball_node = FIREBALL.instantiate() 
+	get_parent().add_child(fireball_node)
+	fireball_node.global_position = marker_3d.global_position
