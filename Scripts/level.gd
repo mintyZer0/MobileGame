3ae1fb36d9c759fdd89fab57776ad012d2cef306
minty <eithanmathewmalonzo@gmail.com>
2025-02-25 18:27:14 +0800

    This commit fixes the orientation of the slimes and the level to face the correct forward direction which is -z - changed the orientation of the slimes in the SlimesReworked file to face -Y - replaced the current player slimes with the new imported models SlimesReworked - Since the orientation of slimes on blend is y+ in order to align the imported slimes forward I needed to add -180 to its y axis - To orient the player slimes properly I had to add a new node3D as the root node of the player slimes then set -180 (do not set -180 axis on the player node only on the node3D which is the root node) - Changed the movement of the players from moving on the z axis to the x axis - Changed the movement of the modules to move backwards by setting it to move towards +z - Changed the spawning of the modules to move towards -z by setting its position.z to -position.z - Changed the direction of the fireballs to move towards the z axis, which means setting the .global_transform.basis.z.normalized() to positive (I do not know why I had to put positive) - Increased the player spawn area space from each other to prevent them from spawning on top of one another - Fixed slime_player hitbox so they dont collide accidentally and go beyond the stratosphere

--- Scripts/level.gd
+++ Scripts/level.gd
@@ -30,14 +30,14 @@ func spawn_slimes():
 		match slime:
 			"MiddleSlime":
 				slime_instance.global_position = spawn_markers[0].global_position
-				slime_instance.global_rotation_degrees = Vector3(0,-90,0)
-				add_camera(slime_instance)
+				
+				add_camera(slime_instance.get_child(0))
 			"LeftSlime":
 				slime_instance.global_position = spawn_markers[1].global_position
-				slime_instance.global_rotation_degrees = Vector3(0,-90,0)
+
 			"RightSlime":
 				slime_instance.global_position = spawn_markers[2].global_position
-				slime_instance.global_rotation_degrees = Vector3(0,-90,0)
+
 				
 func add_camera(player):
 	var camera_instance = camera.instantiate()
@@ -46,12 +46,14 @@ func add_camera(player):
 #Spawns empty module in the beginning so that you dont die immediately
 func spawnEmptyModule(n):
 	var empty_module = modules[0].instantiate()
-	empty_module.position.z = abs(-n)
+	empty_module.position.z = n
+	empty_module.position.z = -empty_module.position.z
 	add_child(empty_module)
 
 func spawnModule(n):
 	rng.randomize()
 	var num = rng.randi_range(0, modules.size()-1)
 	var instance = modules[num].instantiate()
-	instance.position.z = abs(-n)
+	instance.position.z = n
+	instance.position.z = -instance.position.z
 	add_child(instance)
