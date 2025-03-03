51f1d563ce3682e83bdf1cdf9cf4f145879668a4
minty <eithanmathewmalonzo@gmail.com>
2025-03-03 15:11:50 +0800

    Added jump feature using tap and lowered sensitivity of slime touch drag movement

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,17 +1,45 @@
 extends CharacterBody3D
 
 const SPEED = 5.0
-const SENSITIVITY = 4.0
+const SENSITIVITY = 0.01
 const JUMP_VELOCITY = 4.5
+var tap_threshold = 20 # Maximum movement in pixels to consider it a tap
 @export var selected = false
 @onready var player = $"."
+var dragging = false
+var initial_position = Vector2.ZERO
 
 func _input(event: InputEvent) -> void:
-	if event is InputEventScreenDrag:
+	#if event is a touch input
+	if event is InputEventScreenTouch:
+		#check if it is pressed, assume it is not dragging
+		if event.is_pressed():
+			dragging = false
+			initial_position = event.position
+		# compare initial position of tap to the current position
+		# if the position difference is not too much the consider it as a tap, else do not tap
+		else:
+			if not dragging:
+				if event.position.distance_to(initial_position) <= tap_threshold:
+					print("tap")
+					jump()
+
+	elif event is InputEventScreenDrag:
 		handle_screen_drag(event)
 	
+func _timer_start():
+	var timer = Timer.new()
+	timer.wait_time = 0.1
+	timer.one_shot = true
+	add_child(timer)
+	timer.connect("timeout", Callable(self, "_on_timer_end"))
+	timer.start()
+
+func _on_timer_end():
+	return true
 
 func _ready() -> void:
+	
 	add_to_group("player")
 
 func _physics_process(delta: float) -> void:
@@ -39,6 +67,9 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 
 func handle_screen_drag(event):
-	print(event.relative)
 	var direction = event.relative
-	velocity.x = direction.x * SENSITIVITY
+	position.x -= direction.x * SENSITIVITY
+
+func jump():
+	if is_on_floor():
+		velocity.y = JUMP_VELOCITY
